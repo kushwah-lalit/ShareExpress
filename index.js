7fe6ca9ee284bd02bb0f51860ed824e83c2a872a
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-05-25 22:02:51 +0530

    first commit: Made directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.listen(port,function(error){
+    if(error){
+        console.log(`Error occured while running the server: ${error}`);
+    }
+    console.log(`Server running on port: ${port}`);
+});
