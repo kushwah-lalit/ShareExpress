ff2e6289de77dc8500a04bd2f981fa795ac162ba
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-05-26 01:41:17 +0530

    setup the view engine with ejs file

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>User lalit here :-)</h1>');
+    // return res.end('<h1>User lalit here :-)</h1>');
+    return res.render('user_profile',{
+        title:"Profile Page"
+    });
 };
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = 8000;
 // use routes for the req
 app.use('/',require('./routes/index'));
 // setup the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port,function(error){
     if(error){
         console.log(`Error occured while running the server: ${error}`);
