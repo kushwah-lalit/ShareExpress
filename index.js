57b90c941811929d62013edba27ef71b8a101c36
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-05-26 01:23:14 +0530

    setup the view engine

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 // use routes for the req
 app.use('/',require('./routes/index'));
+// setup the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(error){
     if(error){
         console.log(`Error occured while running the server: ${error}`);
