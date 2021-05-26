1f69144808a425ddce454da99065b35134b71595
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-05-26 12:51:55 +0530

    adding the style and script from the different files to the layout being used

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ app.use(express.static('./assets'));
 //should be before the routes
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+// Putting the styles and scripts from pages to the correct place in the layout file
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 // use routes for the req
 app.use('/',require('./routes/index'));
-// setup the view engine
+// setup the view engine 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.listen(port,function(error){
