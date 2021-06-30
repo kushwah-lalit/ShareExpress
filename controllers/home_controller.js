a515f4a88da86d4f0c0f1930c3b52b249b1a532b
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-06-30 21:48:33 +0530

    Made changes to the home_controller for populating the coment->user, and Made changes to hoe.ejs to show the comments and their users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,23 @@ module.exports.home=function(req,res){
     // });
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+    // Post.find({}).populate('user').exec(function(err, posts){
+    //     return res.render('home', {
+    //         title: "ShareExpress | Home",
+    //         posts:  posts
+    //     });
+    // })
+    // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        // how to populate nested
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
             title: "ShareExpress | Home",
             posts:  posts
