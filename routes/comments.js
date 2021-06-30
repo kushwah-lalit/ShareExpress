1a8ef03c53b36909aa88e75204bef24e7d0f9401
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-06-30 19:55:32 +0530

    Add routes to make comment, setup its controller, modified the home.ejs to show the form for the signed in user to comment on post

---
+++ routes/comments.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('passport');
+
+const commentController = require('../controllers/comments_controller');
+console.log('Router file working');
+router.post('/create', passport.checkAuthentication,commentController.create);
+
+module.exports=router; 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ console.log('Router file working');
 router.get('/',homeController.home);
 router.use('/users',require('./users')); 
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
