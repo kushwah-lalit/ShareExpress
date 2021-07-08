ee06f54dd2e03698899fba53a91eae5f3986f338
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-07-09 01:41:10 +0530

    modified home controller....added asyn await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post =require('../models/post');
 const User =require('../models/user');
-module.exports.home=function(req,res){
+module.exports.home= async function(req,res){
     // return res.end('<h1>Hi lalit here :-)</h1>');
     // console.log(req.cookie);
     // return res.render('home',{
@@ -21,24 +21,52 @@ module.exports.home=function(req,res){
     //         posts:  posts
     //     });
     // })
+// ***************************new********
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        // how to populate nested
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        User.find({},function(err,users){
-            return res.render('home', {
-                title: "ShareExpress | Home",
-                posts:  posts,
-                all_users: users
-            });
-        });
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     // how to populate nested
+    //     path: 'comments',
+    //     populate: {
+    //         path: 'user'
+    //     }
+    // })
+    // .exec(function(err, posts){
+    //     // populate user to show on friends
+    //     User.find({},function(err,users){
+    //         return res.render('home', {
+    //             title: "ShareExpress | Home",
+    //             posts:  posts,
+    //             all_users: users
+    //         });
+    //     });
         
-    })
+    // })
+// ********************new code with async awaits*****************
+        try{
+         // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "ShareExpress| Home",
+            posts:  posts,
+            all_users: users
+        });
+
+        }catch(err){
+        console.log('Error', err);
+        return;
+        }
+
+
 }; 
