ea2ebc69992da519c2c8feef301ff843fda920b7
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-08-07 16:18:48 +0530

    setup the nodemailer to notify the user about the activity via email...also setup the kue and redis for the delayed jobs

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
 //     Post.findById(req.body.post, function(err, post){
@@ -36,10 +39,21 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+            // need to populate outside for both kind of requests also populate the email
+            comment = await comment.populate('user', 'name email').execPopulate();
+            // commentsMailer.newComment(comment);
 
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
+
+            });
             if (req.xhr){
                 // Similar for comments to fetch the user's id!
-                comment = await comment.populate('user', 'name').execPopulate();
+                // comment = await comment.populate('user', 'name').execPopulate();
     
                 return res.status(200).json({
                     data: {
---
+++ mailers/comments_mailer.js
@@ -0,0 +1,26 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+// this is another way of exporting a method
+exports.newComment = (comment) => {
+    console.log('inside newComment mailer', comment);
+    // now we will be sending via template
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+       from: 'projectmailer.tester@gmail.com',
+       to: comment.user.email,
+       subject: "New Comment Published!",
+    //    html: '<h1>Yup, your comment is now published!</h1>' 
+       html: htmlString
+    
+    }, (err, info) => {
+        if (err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+
+        console.log('Message sent', info);
+        return;
+    });
+}
