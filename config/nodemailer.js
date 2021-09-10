68a5d55bf43d3e8425dfc906cf1d4bc264073ce6
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-09-10 20:34:34 +0530

    added like schema also modified the post and comment schema - POLYMORRPHIC relationship

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -10,7 +10,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'projectmailer.tester@gmail.com',
-        // pass: ''hiding 
+        pass: 'qwerty!@#$%'
     }
 });
 
--- models/post.js
+++ models/post.js
@@ -14,6 +14,12 @@ const postSchema = new mongoose.Schema({
             type:  mongoose.Schema.Types.ObjectId,
             ref:'Comment'
         }
+    ],
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
+        }
     ]
 },
 {
