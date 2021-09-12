932f80db20a4dc296ca1649743ca8e22500e5394
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-09-12 19:09:04 +0530

    setted up the production environment also steup the bash to store the keys value...Also setup the logging setup for the production mode

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
 
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: logDirectory
+});
 
 const development = {
     name: 'development',
@@ -19,13 +30,38 @@ const development = {
     google_client_secret: 'zS0XsrCmch_mBidsp6bmPx3f',
     google_call_back_url: "http://localhost:8000/users/auth/google/callback",
     jwt_secret: 'codeial',
+    morgan: {
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    }
 }
 
 
 const production =  {
-    name: 'production'
+    name: 'production',
+    asset_path: process.env.ShareExpress_ASSET_PATH,
+    session_cookie_key: process.env.ShareExpress_SESSION_COOKIE_KEY,
+    db: process.env.ShareExpress_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.ShareExpress_GMAIL_USERNAME,
+            pass: process.env.ShareExpress_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.ShareExpress_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.ShareExpress_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.ShareExpress_GOOGLE_CALL_BACK_URL,
+    jwt_secret: process.env.ShareExpress_JWT_SECRET,
+    morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
 }
 
 
 
-module.exports = development;
+module.exports = eval(process.env.ShareExpress_ENVIRONMENT) == undefined ? development : eval(process.env.ShareExpress_ENVIRONMENT);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const env = require('./config/environment');
+const logger = require('morgan');
 // app.use(express.static('./assets'));@@@@
 app.use(express.static(env.asset_path));
 //should be before the routes
@@ -31,17 +32,27 @@ console.log('chat server is listening on port 5000');
 
 const path = require('path');
 
-app.use(sassMiddleware({
-    // src: './assets/scss',
-    // dest: './assets/css',@@@@
-    src: path.join(__dirname, env.asset_path, 'scss'),
-    dest: path.join(__dirname, env.asset_path, 'css'),
-    debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+// app.use(sassMiddleware({
+//     // src: './assets/scss',
+//     // dest: './assets/css',@@@@
+//     src: path.join(__dirname, env.asset_path, 'scss'),
+//     dest: path.join(__dirname, env.asset_path, 'css'),
+//     debug: true,
+//     outputStyle: 'extended',
+//     prefix: '/css'
+// }));
+if (env.name == 'development'){
+    app.use(sassMiddleware({
+        src: path.join(__dirname, env.asset_path, 'scss'),
+        dest: path.join(__dirname, env.asset_path, 'css'),
+        debug: true,
+        outputStyle: 'extended',
+        prefix: '/css'
+    }));
+}
 // to make upload path available to the browser
 app.use('/uploads',express.static(__dirname + '/uploads'));
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts); 
 app.use(express.urlencoded());
